ee5c495411944096c238c372d6471459d5c0636a
ianbautista <ianfbautista@gmail.com>
2020-07-17 08:24:50 -0700

    added slices of state. fetched characters from Pokemon API using effect hook.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+	// Try to think through what state you'll need for this app before starting. Then build out
+	// the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+	const [pokemon, setPokemon] = useState([]);
+	const [previous, setPrevious] = useState("");
+	const [next, setNext] = useState("");
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
+	// Fetch characters from the API in an effect hook. Remember, anytime you have a
+	// side effect in a component, you want to think about which state and/or props it should
+	// sync up with, if any.
+
+	useEffect(() => {
+		axios
+			.get("https://pokeapi.co/api/v2/pokemon")
+			.then((response) => {
+				setPokemon(response.data.results);
+				setPrevious(response.data.previous);
+				setNext(response.data.next);
+			})
+			.catch((error) => {
+				console.log(`Oops!! Team Rocket is here! Causing ${error}`);
+			});
+	}, []);
+
+	return (
+		<div className="App">
+			<h1 className="Header">Characters</h1>
+		</div>
+	);
+};
 
 export default App;
