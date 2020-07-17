ae46cd34287a206c00c9e2ca1729a571a998e8ab
ianbautista <ianfbautista@gmail.com>
2020-07-17 09:55:02 -0700

    added animation to pokemons. Stretch done!

--- starwars/src/components/styledComponents.js
+++ starwars/src/components/styledComponents.js
@@ -6,12 +6,62 @@ export const PokemonCard = styled.div`
 	background-color: white;
 	width: 150px;
 	margin: 1rem;
-	border-radius: 10%;
-	h2 {
-		font-size: 1rem;
-		padding: 0.5rem;
-		color: dodgerblue;
+    border-radius: 10%;
+    
+    /* animation stretch */
+    @keyframes wiggle {
+		0% {
+			transform: skewX(9deg);
+		}
+		10% {
+			transform: skewX(-8deg);
+		}
+		20% {
+			transform: skewX(7deg);
+		}
+		30% {
+			transform: skewX(-6deg);
+		}
+		40% {
+			transform: skewX(5deg);
+		}
+		50% {
+			transform: skewX(-4deg);
+		}
+		60% {
+			transform: skewX(3deg);
+		}
+		70% {
+			transform: skewX(-2deg);
+		}
+		80% {
+			transform: skewX(1deg);
+		}
+		90% {
+			transform: skewX(0deg);
+		}
+		100% {
+			transform: skewX(0deg);
+		}
 	}
+
+    h2 {
+        font-size: 1rem;
+        padding: 0.5rem;
+        color: dodgerblue;
+    }
+    
+    &:hover {
+		h2 {
+			display: none;
+        }
+        img {
+		animation-name: wiggle;
+		animation-timing-function: ease-in;
+		animation-duration: 0.5s;
+		animation-iteration-count: infinite;
+	}
+
 `;
 
 // setting button styling
